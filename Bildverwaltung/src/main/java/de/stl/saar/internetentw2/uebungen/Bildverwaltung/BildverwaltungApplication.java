f9706c50836e6d051a5251349689061e5af6b00c
jogerwert <jogerwert@web.de>
2019-08-10 20:22:16 +0200

    Spring Boot

---
+++ Bildverwaltung/src/main/java/de/stl/saar/internetentw2/uebungen/Bildverwaltung/BildverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.stl.saar.internetentw2.uebungen.Bildverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BildverwaltungApplication.class, args);
+	}
+
+}
